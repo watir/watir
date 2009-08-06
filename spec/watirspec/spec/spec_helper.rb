524db96e78903f1c11d5c34581f6e3b2c9bcb835
Jari Bakken <jari.bakken@gmail.com>
2009-08-06 23:00:30 +0200

    Use .rb since we're checking if File.exist?

--- spec/watirspec/spec/spec_helper.rb
+++ spec/watirspec/spec/spec_helper.rb
@@ -11,7 +11,7 @@ end
 Thread.abort_on_exception = true
 
 require "#{File.dirname(__FILE__)}/../watirspec"
-hook = File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
+hook = File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper.rb")
 
 if File.exist?(hook)
   require hook 
