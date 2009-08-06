cdf5c0a34ba2a718552e262be109a3d1db9c3d65
Jari Bakken <jari.bakken@gmail.com>
2009-08-06 23:12:59 +0200

    Fix authentication spec

--- spec/watirspec/spec/browser_authentication_spec.rb
+++ spec/watirspec/spec/browser_authentication_spec.rb
@@ -8,13 +8,13 @@ describe "Browser" do
 
   describe "#credentials=" do
     it "sets the basic authentication credentials" do
-      @browser.goto(WatirSpec.host + "/authentication/")
-      @browser.text.should_not include("Index of /authentication/")
+      @browser.goto(WatirSpec.host + "/authentication")
+      @browser.text.should_not include("ok")
 
       @browser.credentials = "foo:bar"
 
-      @browser.goto(WatirSpec.host + "/authentication/")
-      @browser.text.should include("Index of /authentication/")
+      @browser.goto(WatirSpec.host + "/authentication")
+      @browser.text.should include("ok")
     end
   end
 end
