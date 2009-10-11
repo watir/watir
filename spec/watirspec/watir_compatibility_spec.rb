fc07db532978c3ead4e5981a9589333ac6c5f97c
Jari Bakken <jari.bakken@gmail.com>
2009-10-12 00:34:41 +0200

    Remove Celerity spec

--- spec/watirspec/watir_compatibility_spec.rb
+++ spec/watirspec/watir_compatibility_spec.rb
@@ -63,36 +63,6 @@ describe "IE" do
 
 end
 
-describe "Button" do
-
-  before :each do
-    browser.goto(WatirSpec.host + "/forms_with_input_elements.html")
-  end
-
-  describe "#click_no_wait" do
-    it "behaves like #click" do
-      browser.button(:id, 'new_user_submit').click_no_wait
-      browser.text.should include("You posted the following content:")
-    end
-  end
-
-end
-
-describe "Link" do
-
-  before :each do
-    browser.goto(WatirSpec.host + "/non_control_elements.html")
-  end
-
-  describe "#click_no_wait" do
-    it "behaves like #click" do
-      browser.link(:text, "Link 3").click_no_wait
-      browser.text.include?("User administration").should be_true
-    end
-  end
-
-end
-
 describe "Image" do
 
   before :each do
