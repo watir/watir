a8daacd5b18891c5221e89f7362857c7a3e645ea
Jari Bakken <jari.bakken@gmail.com>
2009-11-26 03:59:21 +0100

    Fix for Image#{width,height}
    Ff the attribute isn't set, we want the actual values.

--- lib/watir/elements/image.rb
+++ lib/watir/elements/image.rb
@@ -2,6 +2,16 @@
 module Watir
   class Image < HTMLElement
 
+    def width
+      assert_exists
+      driver.execute_script "return arguments[0].width", @element
+    end
+
+    def height
+      assert_exists
+      driver.execute_script "return arguments[0].height", @element
+    end
+
     def file_created_date
       assert_exists
       raise NotImplementedError
