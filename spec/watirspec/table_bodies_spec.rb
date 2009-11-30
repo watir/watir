361df089c7b1895da3954a3eaea2d4422a1899ce
Jari Bakken <jari.bakken@gmail.com>
2009-11-30 19:27:04 +0100

    Container#{body,bodies} => Container#{tbody,tbodys}

--- spec/watirspec/table_bodies_spec.rb
+++ spec/watirspec/table_bodies_spec.rb
@@ -9,35 +9,35 @@ describe "TableBodies" do
 
   describe "#length" do
     it "returns the correct number of table bodies (page context)" do
-      browser.bodies.length.should == 5
+      browser.tbodys.length.should == 5
     end
 
     it "returns the correct number of table bodies (table context)" do
-      browser.table(:index, 0).bodies.length.should == 2
+      browser.table(:index, 0).tbodys.length.should == 2
     end
   end
 
   describe "#[]" do
     it "returns the row at the given index (page context)" do
-      browser.bodies[0].id.should == "first"
+      browser.tbodys[0].id.should == "first"
     end
 
     it "returns the row at the given index (table context)" do
-      browser.table(:index, 0).bodies[0].id.should == "first"
+      browser.table(:index, 0).tbodys[0].id.should == "first"
     end
   end
 
   describe "#each" do
       it "iterates through table bodies correctly (table context)" do
-        browser.bodies.each_with_index do |body, index|
+        browser.tbodys.each_with_index do |body, index|
           body.id.should == browser.tbody(:index, index).id
         end
       end
 
       it "iterates through table bodies correctly (table context)" do
         table = browser.table(:index, 0)
-        table.bodies.each_with_index do |body, index|
-          body.id.should == table.body(:index, index).id
+        table.tbodys.each_with_index do |body, index|
+          body.id.should == table.tbody(:index, index).id
         end
       end
     end
--- spec/watirspec/table_body_spec.rb
+++ spec/watirspec/table_body_spec.rb
@@ -10,82 +10,82 @@ describe "TableBody" do
   describe "#exists?" do
     bug "WTR-357", :watir do
       it "returns true if the table body exists (page context)" do
-        browser.body(:id, 'first').should exist
-        browser.body(:id, /first/).should exist
-        browser.body(:index, 0).should exist
-        browser.body(:xpath, "//tbody[@id='first']").should exist
+        browser.tbody(:id, 'first').should exist
+        browser.tbody(:id, /first/).should exist
+        browser.tbody(:index, 0).should exist
+        browser.tbody(:xpath, "//tbody[@id='first']").should exist
       end
     end
 
     it "returns true if the table body exists (table context)" do
-      browser.table(:index, 0).body(:id, 'first').should exist
-      browser.table(:index, 0).body(:id, /first/).should exist
-      browser.table(:index, 0).body(:index, 1).should exist
-      browser.table(:index, 0).body(:xpath, "//tbody[@id='first']").should exist
+      browser.table(:index, 0).tbody(:id, 'first').should exist
+      browser.table(:index, 0).tbody(:id, /first/).should exist
+      browser.table(:index, 0).tbody(:index, 1).should exist
+      browser.table(:index, 0).tbody(:xpath, "//tbody[@id='first']").should exist
     end
 
     bug "WTR-357", :watir do
       it "returns false if the table body doesn't exist (page context)" do
-        browser.body(:id, 'no_such_id').should_not exist
-        browser.body(:id, /no_such_id/).should_not exist
-        browser.body(:index, 1337).should_not exist
-        browser.body(:xpath, "//tbody[@id='no_such_id']").should_not exist
+        browser.tbody(:id, 'no_such_id').should_not exist
+        browser.tbody(:id, /no_such_id/).should_not exist
+        browser.tbody(:index, 1337).should_not exist
+        browser.tbody(:xpath, "//tbody[@id='no_such_id']").should_not exist
       end
     end
 
     it "returns false if the table body doesn't exist (table context)" do
-      browser.table(:index, 0).body(:id, 'no_such_id').should_not exist
-      browser.table(:index, 0).body(:id, /no_such_id/).should_not exist
-      browser.table(:index, 0).body(:index, 1337).should_not exist
-      browser.table(:index, 0).body(:xpath, "//tbody[@id='no_such_id']").should_not exist
+      browser.table(:index, 0).tbody(:id, 'no_such_id').should_not exist
+      browser.table(:index, 0).tbody(:id, /no_such_id/).should_not exist
+      browser.table(:index, 0).tbody(:index, 1337).should_not exist
+      browser.table(:index, 0).tbody(:xpath, "//tbody[@id='no_such_id']").should_not exist
     end
 
     it "raises TypeError when 'what' argument is invalid" do
       bug "WTR-357", :watir do
-        lambda { browser.body(:id, 3.14).exists? }.should raise_error(TypeError)
+        lambda { browser.tbody(:id, 3.14).exists? }.should raise_error(TypeError)
       end
-      lambda { browser.table(:index, 0).body(:id, 3.14).exists? }.should raise_error(TypeError)
+      lambda { browser.table(:index, 0).tbody(:id, 3.14).exists? }.should raise_error(TypeError)
     end
 
     it "raises MissingWayOfFindingObjectException when 'how' argument is invalid" do
       bug "WTR-357", :watir do
-        lambda { browser.body(:no_such_how, 'some_value').exists? }.should raise_error(MissingWayOfFindingObjectException)
+        lambda { browser.tbody(:no_such_how, 'some_value').exists? }.should raise_error(MissingWayOfFindingObjectException)
       end
-      lambda { browser.table(:index, 0).body(:no_such_how, 'some_value').exists? }.should raise_error(MissingWayOfFindingObjectException)
+      lambda { browser.table(:index, 0).tbody(:no_such_how, 'some_value').exists? }.should raise_error(MissingWayOfFindingObjectException)
     end
   end
 
   describe "#length" do
     bug "WTR-357", :watir do
       it "returns the correct number of table bodies (page context)" do
-        browser.body(:id, 'first').length.should == 3
+        browser.tbody(:id, 'first').length.should == 3
       end
     end
 
     it "returns the correct number of table bodies (table context)" do
-      browser.table(:index, 0).body(:id, 'first').length.should == 3
+      browser.table(:index, 0).tbody(:id, 'first').length.should == 3
     end
   end
 
   describe "#[]" do
     bug "WTR-357", :watir do
       it "returns the row at the given index (page context)" do
-        browser.body(:id, 'first')[0].text.should == 'March 2008'
-        browser.body(:id, 'first')[1][0].text.should == 'Gregory House'
-        browser.body(:id, 'first')[2][0].text.should == 'Hugh Laurie'
+        browser.tbody(:id, 'first')[0].text.should == 'March 2008'
+        browser.tbody(:id, 'first')[1][0].text.should == 'Gregory House'
+        browser.tbody(:id, 'first')[2][0].text.should == 'Hugh Laurie'
       end
     end
 
     it "returns the row at the given index (table context)" do
-      browser.table(:index, 0).body(:id, 'first')[0].text.should == 'March 2008'
-      browser.table(:index, 0).body(:id, 'first')[1][0].text.should == 'Gregory House'
-      browser.table(:index, 0).body(:id, 'first')[2][0].text.should == 'Hugh Laurie'
+      browser.table(:index, 0).tbody(:id, 'first')[0].text.should == 'March 2008'
+      browser.table(:index, 0).tbody(:id, 'first')[1][0].text.should == 'Gregory House'
+      browser.table(:index, 0).tbody(:id, 'first')[2][0].text.should == 'Hugh Laurie'
     end
   end
 
   describe "#each" do
     it "iterates through rows correctly" do
-      body = browser.table(:index, 0).body(:id, 'first')
+      body = browser.table(:index, 0).tbody(:id, 'first')
       expected_texts = ["march", "gregory", "hugh"]
 
       body.each_with_index do |row, idx|
