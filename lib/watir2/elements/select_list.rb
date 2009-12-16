c7dac51a3a6d93256c890739073fbb460d077bb0
Jari Bakken <jari.bakken@gmail.com>
2009-12-16 02:29:02 +0100

    Fix some select list specs - 90% pass!

--- lib/watir2/elements/select_list.rb
+++ lib/watir2/elements/select_list.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 module Watir
   class SelectList < Select
+    include Watir::Exception
 
     container_method  :select_list
     collection_method :select_lists
@@ -23,27 +24,20 @@ module Watir
     end
 
     def select(str_or_rx)
-      assert_exists
-      os = options.select { |e| str_or_rx === e.text }
-
-      if os.empty?
-        raise NoValueFoundException, "#{str_or_rx.inspect} not found in select list"
-      end
-
-      os.each { |e| e.select unless e.selected? }
-      os.first.text
+      select_by :text, str_or_rx
     end
 
     # TODO: deprecate?
-    def select_value(val)
-      option(:value, val).select
+    def select_value(str_or_rx)
+      select_by :value, str_or_rx
     end
 
     def selected?(str_or_rx)
       assert_exists
       matches = options.select { |e| str_or_rx === e.text }
+
       if matches.empty?
-        raise UnknownObjectExcpetion, "Unable to locate option matching #{str_or_rx.inspect}"
+        raise UnknownObjectException, "Unable to locate option matching #{str_or_rx.inspect}"
       end
 
       matches.any? { |e| e.selected? }
@@ -59,6 +53,20 @@ module Watir
     def selected_options
       assert_exists
       options.map { |e| e.text if e.selected? }.compact
+    end
+
+    private
+
+    def select_by(how, str_or_rx)
+      assert_exists
+      os = options.select { |e| str_or_rx === e.send(how) }
+
+      if os.empty?
+        raise NoValueFoundException, "#{str_or_rx.inspect} not found in select list"
+      end
+
+      os.each { |e| e.select unless e.selected? }
+      os.first.text
     end
 
   end
