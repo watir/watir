55352afa318df497a790c65a212f1821c1aa0112
Jari Bakken <jari.bakken@gmail.com>
2010-06-16 17:15:41 +0200

    Add Nokogiri extension.
    This extension monkey patches Browser#text to use Nokogiri, which is faster than
    WebDriver's cross-browser JS solution. The tradeoff is that #text may not be
    the same for all browsers.

---
+++ lib/watir-webdriver/extensions/nokogiri.rb
@@ -0,0 +1,14 @@
+require "nokogiri"
+
+module Watir
+  class Browser
+
+    def text
+      doc = Nokogiri::HTML(body(:index => 0).html)
+      doc.css("script").remove # encoding bug in older libxml?
+
+      doc.inner_text
+    end
+
+  end
+end
