96b6e8f6aeba9e4412c94f5fc2b1b87f6117e564
Jari Bakken <jari.bakken@gmail.com>
2010-06-16 18:55:33 +0200

    Fix some xpaths

--- lib/watir-webdriver/elements/select_list.rb
+++ lib/watir-webdriver/elements/select_list.rb
@@ -182,9 +182,9 @@ module Watir
     def option_xpath_for(how, string)
       case how
       when :text
-        "//option[normalize-space()='#{string}' or @label='#{string}']"
+        ".//option[normalize-space()='#{string}' or @label='#{string}']"
       when :value
-        "//option[@value='#{string}']"
+        ".//option[@value='#{string}']"
       else
         raise Error, "unknown how: #{how.inspect}"
       end
