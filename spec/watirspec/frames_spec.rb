39664ec5c34ee31a221026d80f83287ef811ac61
Jari Bakken <jari.bakken@gmail.com>
2010-08-20 16:28:18 +0200

    Guard all of frames_spec.rb with http://github.com/jarib/watir-webdriver/issues#issue/17

--- spec/watirspec/frames_spec.rb
+++ spec/watirspec/frames_spec.rb
@@ -1,79 +1,75 @@
 # encoding: utf-8
 require File.expand_path('spec_helper', File.dirname(__FILE__))
 
-describe "<frame> Frames" do
+bug "http://github.com/jarib/watir-webdriver/issues#issue/17", :webdriver do
 
-  before :each do
-    browser.goto(WatirSpec.files + "/frames.html")
-  end
+  describe "<frame> Frames" do
 
-  bug "http://github.com/jarib/watir-webdriver/issues#issue/17", :webdriver do
+    before :each do
+      browser.goto(WatirSpec.files + "/frames.html")
+    end
+
     describe "#length" do
       it "returns the correct number of frames" do
         browser.frames.length.should == 2
       end
     end
-  end
 
-  bug "http://github.com/jarib/watir-webdriver/issues#issue/17", :webdriver do
     describe "#[]" do
       it "returns the frame at the given index" do
         browser.frames[0].id.should == "frame_1"
       end
     end
-  end
 
-  describe "#each" do
-    it "iterates through frames correctly" do
-      count = 0
+    describe "#each" do
+      it "iterates through frames correctly" do
+        count = 0
 
-      browser.frames.each_with_index do |f, index|
-        f.id.should ==  browser.frame(:index, index).id
-        f.value.should == browser.frame(:index, index).value
+        browser.frames.each_with_index do |f, index|
+          f.id.should ==  browser.frame(:index, index).id
+          f.value.should == browser.frame(:index, index).value
 
-        count += 1
+          count += 1
+        end
+
+        count.should > 0
       end
-
-      count.should > 0
     end
   end
-end
 
-describe "<iframe> Frames" do
+  describe "<iframe> Frames" do
 
-  before :each do
-    browser.goto(WatirSpec.files + "/iframes.html")
-  end
+    before :each do
+      browser.goto(WatirSpec.files + "/iframes.html")
+    end
 
-  bug "http://github.com/jarib/watir-webdriver/issues#issue/17", :webdriver do
     describe "#length" do
       it "returns the correct number of frames" do
         browser.frames.length.should == 2
       end
     end
-  end
 
-  bug "http://github.com/jarib/watir-webdriver/issues#issue/17", :webdriver do
     describe "#[]" do
       it "returns the frame at the given index" do
         browser.frames[0].id.should == "frame_1"
       end
     end
-  end
 
-  describe "#each" do
-    it "iterates through frames correctly" do
-      count = 0
+    describe "#each" do
+      it "iterates through frames correctly" do
+        count = 0
 
-      browser.frames.each_with_index do |f, index|
-        f.name.should == browser.frame(:index, index).name
-        f.id.should ==  browser.frame(:index, index).id
-        f.value.should == browser.frame(:index, index).value
+        browser.frames.each_with_index do |f, index|
+          f.name.should == browser.frame(:index, index).name
+          f.id.should ==  browser.frame(:index, index).id
+          f.value.should == browser.frame(:index, index).value
 
-        count += 1
+          count += 1
+        end
+
+        count.should > 0
       end
-
-      count.should > 0
     end
   end
+
 end
