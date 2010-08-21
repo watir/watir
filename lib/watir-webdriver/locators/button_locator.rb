b684e86710b7b222d3c6fcd23df8a318bd9241d6
Jari Bakken <jari.bakken@gmail.com>
2010-08-21 21:47:11 +0200

    Temp fix for browser.button.exists? returning false. This needs specs.

--- lib/watir-webdriver/locators/button_locator.rb
+++ lib/watir-webdriver/locators/button_locator.rb
@@ -3,6 +3,22 @@ module Watir
 
     VALID_TYPES = %w[button reset submit image]
 
+    def wd_find_first_by(how, what)
+      if how == :tag_name
+        super(:xpath, ".//button | //input[#{attribute_expression :type => VALID_TYPES}]")
+      else
+        super
+      end
+    end
+
+    def wd_find_all_by(how, what)
+      if how == :tag_name
+        super(:xpath, ".//button | //input[#{attribute_expression :type => VALID_TYPES}]")
+      else
+        super
+      end
+    end
+
     def locate_all
       find_all_by_multiple
     end
