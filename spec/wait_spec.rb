7b137959e0a9d91dc49a71772f36323c75ab0130
Jari Bakken <jari.bakken@gmail.com>
2010-08-30 20:32:15 +0200

    Work around RSpec + 1.9.2 issue with pending specs.

--- spec/wait_spec.rb
+++ spec/wait_spec.rb
@@ -3,26 +3,49 @@ require "watir-webdriver/extensions/wait"
 
 describe Watir::Wait do
   describe "#until" do
-    it "waits until the block returns true"
-    it "times out"
+    it "waits until the block returns true" do
+      pending
+    end
+    
+    it "times out" do
+      pending
+    end
   end
 
   describe "#while" do
-    it "waits while the block returns true"
-    it "times out"
+    it "waits while the block returns true" do
+      pending
+    end
+    
+    it "times out" do
+      pending
+    end
   end
 end
 
 describe Watir::Element do
   describe "#present?" do
-    it "returns true if the element exists and is visible"
-    it "returns false if the element exists but is not visible"
-    it "returns false if the element does not exist"
+    it "returns true if the element exists and is visible" do
+      pending
+    end
+    
+    it "returns false if the element exists but is not visible" do
+      pending
+    end
+    
+    it "returns false if the element does not exist" do
+      pending
+    end
   end
   
   describe "#when_present" do
-    it "invokes subsequent methods after waiting for the element's presence"
-    it "times out"
+    it "invokes subsequent methods after waiting for the element's presence" do
+      pending
+    end
+    
+    it "times out" do 
+      pending
+    end
   end
   
 end
