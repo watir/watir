f64820bb728f60a2939457f4f8cb8c325f701e94
Jari Bakken <jari.bakken@gmail.com>
2010-09-06 19:09:18 +0200

    Nesting spec for TableSections as well.

--- spec/watirspec/table_nesting_spec.rb
+++ spec/watirspec/table_nesting_spec.rb
@@ -7,8 +7,8 @@ describe "Table" do
     browser.goto(WatirSpec.files + "/nested_tables.html")
   end
 
-  it "reports the correct number of rows for unusual nesting" do
-    tables = browser.div(:id => "rows-test").tables(:id => /^tbl/)
+  it "returns the correct number of rows under a table" do
+    tables = browser.div(:id => "table-rows-test").tables(:id => /^tbl/)
     tables.length.should > 0
 
     tables.each do |table|
@@ -19,11 +19,11 @@ describe "Table" do
       actual.should eql(expected), "expected #{expected} rows, got #{actual} for table id=#{table.id}, browser reported: #{browser_count}"
     end
   end
-  
-  it "reports the correct number of rows" do
-    rows = browser.div(:id => "cells-test").trs(:id => /^row/)
+
+  it "returns the correct number of cells under a row" do
+    rows = browser.div(:id => "row-cells-test").trs(:id => /^row/)
     rows.length.should > 0
-    
+
     rows.each do |row|
       expected      = Integer(row.data_cell_count)
       actual        = row.cells.length
@@ -32,4 +32,19 @@ describe "Table" do
       actual.should eql(expected), "expected #{expected} cells, got #{actual} for row id=#{row.id}, browser reported: #{browser_count}"
     end
   end
+
+  it "returns the correct number of rows under a table section" do
+    tbodies = browser.table(:id => "tbody-rows-test").tbodys(:id => /^body/)
+    tbodies.length.should > 0
+
+    tbodies.each do |tbody|
+      expected      = Integer(tbody.data_rows_count)
+      actual        = tbody.rows.count
+      browser_count = Integer(tbody.data_browser_count)
+
+      actual.should eql(expected), "expected #{expected} rows, got #{actual} for tbody id=#{tbody.id}, browser reported: #{browser_count}"
+    end
+  end
+
+
 end
