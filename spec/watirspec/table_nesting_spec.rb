71b436310d38d22f7bb537125790bd8184f86757
Jari Bakken <jari.bakken@gmail.com>
2010-09-06 18:03:58 +0200

    Similar spec for cells()

--- spec/watirspec/table_nesting_spec.rb
+++ spec/watirspec/table_nesting_spec.rb
@@ -8,15 +8,28 @@ describe "Table" do
   end
 
   it "reports the correct number of rows for unusual nesting" do
-    tables = browser.tables(:id => /^tbl/)
+    tables = browser.div(:id => "rows-test").tables(:id => /^tbl/)
     tables.length.should > 0
 
     tables.each do |table|
-      expected      = Integer(table.data_rowcount)
+      expected      = Integer(table.data_row_count)
       actual        = table.rows.length
-      browser_count = Integer(table.data_browsercount)
+      browser_count = Integer(table.data_browser_count)
 
       actual.should eql(expected), "expected #{expected} rows, got #{actual} for table id=#{table.id}, browser reported: #{browser_count}"
+    end
+  end
+  
+  it "reports the correct number of rows" do
+    rows = browser.div(:id => "cells-test").trs(:id => /^row/)
+    rows.length.should > 0
+    
+    rows.each do |row|
+      expected      = Integer(row.data_cell_count)
+      actual        = row.cells.length
+      browser_count = Integer(row.data_browser_count)
+
+      actual.should eql(expected), "expected #{expected} cells, got #{actual} for row id=#{row.id}, browser reported: #{browser_count}"
     end
   end
 end
