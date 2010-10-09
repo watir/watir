cb72d41e0fdb6ec89aaee28ad5744e6543dc71e9
Jari Bakken <jari.bakken@gmail.com>
2010-10-10 00:37:03 +0200

    Update for new WatirSpec.

---
+++ spec/implementation.rb
@@ -0,0 +1,17 @@
+require File.expand_path("../spec_helper", __FILE__)
+
+WatirSpec.implementation do |imp|
+  name    = :webdriver
+  browser = (ENV['WATIR_WEBDRIVER_BROWSER'] || :firefox).to_sym
+
+  imp.name          = name
+  imp.browser_class = Watir::Browser
+  imp.browser_args  = [browser]
+
+  imp.guard_proc = lambda { |args|
+    args.any? { |arg| arg == name || arg == [name, browser] }
+  }
+end
+
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'rubygems'
 require 'watir-webdriver'
 require 'locator_spec_helper'
+require 'rubygems'
 require 'spec'
 require 'spec/autorun'
 
@@ -12,21 +12,4 @@ include Watir
 include Watir::Exception
 
 WEBDRIVER_SELECTORS = [:class, :class_name, :css, :id, :name, :tag_name, :xpath]
-
-if defined?(WatirSpec)
-  browser = (ENV['WATIR_WEBDRIVER_BROWSER'] || :firefox).to_sym
-
-  WatirSpec.implementation do |imp|
-    imp.name          = :webdriver
-    imp.browser_class = Watir::Browser
-    imp.browser_args  = [browser]
-
-    imp.guard_proc = lambda { |args|
-      args.any? { |arg| arg == :webdriver || arg == [:webdriver, browser] }
-    }
-  end
-
-  # WatirSpec.unguarded = true
-end
-
 
