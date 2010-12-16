658d4f2235853a9498cba7416cba7bd0f4602908
Jari Bakken <jari.bakken@gmail.com>
2010-12-16 14:07:49 +0100

    A hack, but all frame specs are passing. Closes #17.

--- lib/watir-webdriver/elements/frame.rb
+++ lib/watir-webdriver/elements/frame.rb
@@ -5,22 +5,21 @@ module Watir
     def locate
       @parent.assert_exists
 
-      if element = @iframe
-        driver.switch_to.frame @iframe
-      elsif element = @frame
-        switch!
-      else
-        element = locate_iframe || locate_frame
-        element or raise UnknownFrameException, "unable to locate frame/iframe using #{selector_string}"
-      end
+      element = locate_iframe || locate_frame
+      element or raise UnknownFrameException, "unable to locate frame/iframe using #{selector_string}"
 
       element && FramedDriver.new(element, driver)
     end
 
     def assert_exists
-      # we set @element to nil here to make sure the frame is switched
-      # if it's in the selector, that's equally good
-      @element = @selector[:element]
+      if element = @selector[:element]
+        @parent.assert_exists
+        @element = FramedDriver.new(element, driver)
+      else
+        # we set @element to nil here to make sure the frame is switched
+        @element = nil
+      end
+
       super
     end
 
@@ -42,30 +41,13 @@ module Watir
 
     def locate_iframe
       # hack - frame doesn't have IFrame's attributes either
-      @iframe = IFrame.new(@parent, @selector.merge(:tag_name => "iframe")).locate
-
-      if @iframe
-        driver.switch_to.frame @iframe
-        @iframe
-      end
+      IFrame.new(@parent, @selector.merge(:tag_name => "iframe")).locate
     end
 
     def locate_frame
       locator = locator_class.new(@parent.wd, @selector.merge(:tag_name => "frame"), self.class.attribute_list)
-      @frame = locator.locate
-
-      if @frame
-        switch!
-        @frame
-      end
+      locator.locate
     end
-
-    def switch!
-      driver.switch_to.frame @frame
-    rescue Selenium::WebDriver::Error::NoSuchFrameError => e
-      raise UnknownFrameException, e.message
-    end
-
   end # Frame
 
   module Container
@@ -88,13 +70,34 @@ module Watir
       @element = element
       @driver = driver
     end
+    
+    def ==(other)
+      @element == other.element
+    end
+    alias_method :eql?, :==
+    
+    protected
+    
+    def element
+      @element
+    end
+    
+    private
 
     def method_missing(meth, *args, &blk)
       if @driver.respond_to?(meth)
+        switch!
         @driver.send(meth, *args, &blk)
       else
         @element.send(meth, *args, &blk)
       end
     end
+
+    def switch!
+      @driver.switch_to.frame @element
+    rescue Selenium::WebDriver::Error::NoSuchFrameError => e
+      raise UnknownFrameException, e.message
+    end
+
   end # FramedDriver
 end # Watir
