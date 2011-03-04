d545401a779bf2bfc67665d8b6b7757e732ab4c2
Jari Bakken <jari.bakken@gmail.com>
2011-03-04 15:13:08 +0100

    Fix broken spec (found by native events).

--- spec/element_spec.rb
+++ spec/element_spec.rb
@@ -5,8 +5,8 @@ describe Watir::Element do
   describe "#send_keys" do
     it "sends keystrokes to the element" do
       browser.goto("file://" + File.expand_path("html/keylogger.html", File.dirname(__FILE__)))
-      browser.div(:id, 'receiver').send_keys("hello world")
-      browser.div(:id, 'output').ps.size.should == 11
+      browser.element(:id, 'receiver').send_keys("hello world")
+      browser.element(:id, 'output').ps.size.should == 11
     end
   end
 
