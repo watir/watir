9746076529f011fcfe878575a3d33412bd0df101
Jari Bakken <jari.bakken@gmail.com>
2011-03-04 14:54:33 +0100

    Guard some specs on IE.

--- spec/watirspec/table_nesting_spec.rb
+++ spec/watirspec/table_nesting_spec.rb
@@ -7,42 +7,45 @@ describe "Table" do
     browser.goto(WatirSpec.files + "/nested_tables.html")
   end
 
-  it "returns the correct number of rows under a table" do
-    tables = browser.div(:id => "table-rows-test").tables(:id => /^tbl/)
-    tables.length.should > 0
+  # not a webdriver bug - IE seems unable to deal with the invalid nesting
+  not_compliant_on [:webdriver, :ie] do
+    it "returns the correct number of rows under a table" do
+      tables = browser.div(:id => "table-rows-test").tables(:id => /^tbl/)
+      tables.length.should > 0
 
-    tables.each do |table|
-      expected      = Integer(table.data_row_count)
-      actual        = table.rows.length
-      browser_count = Integer(table.data_browser_count)
+      tables.each do |table|
+        expected      = Integer(table.data_row_count)
+        actual        = table.rows.length
+        browser_count = Integer(table.data_browser_count)
 
-      actual.should eql(expected), "expected #{expected} rows, got #{actual} for table id=#{table.id}, browser reported: #{browser_count}"
+        actual.should eql(expected), "expected #{expected} rows, got #{actual} for table id=#{table.id}, browser reported: #{browser_count}"
+      end
     end
-  end
 
-  it "returns the correct number of cells under a row" do
-    rows = browser.div(:id => "row-cells-test").trs(:id => /^row/)
-    rows.length.should > 0
+    it "returns the correct number of cells under a row" do
+      rows = browser.div(:id => "row-cells-test").trs(:id => /^row/)
+      rows.length.should > 0
 
-    rows.each do |row|
-      expected      = Integer(row.data_cell_count)
-      actual        = row.cells.length
-      browser_count = Integer(row.data_browser_count)
+      rows.each do |row|
+        expected      = Integer(row.data_cell_count)
+        actual        = row.cells.length
+        browser_count = Integer(row.data_browser_count)
 
-      actual.should eql(expected), "expected #{expected} cells, got #{actual} for row id=#{row.id}, browser reported: #{browser_count}"
+        actual.should eql(expected), "expected #{expected} cells, got #{actual} for row id=#{row.id}, browser reported: #{browser_count}"
+      end
     end
-  end
 
-  it "returns the correct number of rows under a table section" do
-    tbodies = browser.table(:id => "tbody-rows-test").tbodys(:id => /^body/)
-    tbodies.length.should > 0
+    it "returns the correct number of rows under a table section" do
+      tbodies = browser.table(:id => "tbody-rows-test").tbodys(:id => /^body/)
+      tbodies.length.should > 0
 
-    tbodies.each do |tbody|
-      expected      = Integer(tbody.data_rows_count)
-      actual        = tbody.rows.count
-      browser_count = Integer(tbody.data_browser_count)
+      tbodies.each do |tbody|
+        expected      = Integer(tbody.data_rows_count)
+        actual        = tbody.rows.count
+        browser_count = Integer(tbody.data_browser_count)
 
-      actual.should eql(expected), "expected #{expected} rows, got #{actual} for tbody id=#{tbody.id}, browser reported: #{browser_count}"
+        actual.should eql(expected), "expected #{expected} rows, got #{actual} for tbody id=#{tbody.id}, browser reported: #{browser_count}"
+      end
     end
   end
 
