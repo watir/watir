881f79a2976f88a3253cb34362ca624bed3747e2
Jari Bakken <jari.bakken@gmail.com>
2011-04-06 17:30:10 -0700

    Change selenium-webdriver dependency to 0.2.0.dev, which includes the new Chrome driver.

--- spec/implementation.rb
+++ spec/implementation.rb
@@ -8,12 +8,6 @@ WatirSpec.implementation do |imp|
   imp.browser_class = Watir::Browser
   imp.browser_args  = [browser]
 
-  case browser
-  when :chromedriver
-    # assumes the chromedriver server is running on the default port
-    imp.browser_args = [:remote, {:url => "http://localhost:9515"}]
-  end
-
   imp.guard_proc = lambda { |args|
     args.any? { |arg| arg == name || arg == [name, browser] }
   }
