938f91cf34cdb2b40f5cd4906d93772c464acaa8
Jari Bakken <jari.bakken@gmail.com>
2011-09-22 15:19:32 +0200

    Move specs for Element#send_keys to watirspec.

--- spec/element_spec.rb
+++ spec/element_spec.rb
@@ -2,49 +2,6 @@ require File.expand_path('watirspec/spec_helper', File.dirname(__FILE__))
 
 describe Watir::Element do
 
-  describe '#send_keys' do
-    before(:each) do
-      @c = Selenium::WebDriver::Platform.os == :macosx ? :command : :control
-      browser.goto('file://' + File.expand_path('html/keylogger.html', File.dirname(__FILE__)))
-    end
-
-    let(:receiver) { browser.element(:id => 'receiver')       }
-    let(:events)   { browser.element(:id => 'output').ps.size }
-
-    it 'sends keystrokes to the element' do
-      receiver.send_keys 'hello world'
-      receiver.value.should == 'hello world'
-      events.should == 11
-    end
-
-    it 'accepts arbitrary list of arguments' do
-      receiver.send_keys 'hello', 'world'
-      receiver.value.should == 'helloworld'
-      events.should == 10
-    end
-
-    it 'performs key combinations' do
-      receiver.send_keys 'foo'
-      receiver.send_keys [@c, 'a']
-      receiver.send_keys :backspace
-      receiver.value.should be_empty
-      events.should == 6
-    end
-
-    it 'performs arbitrary list of key combinations' do
-      receiver.send_keys 'foo'
-      receiver.send_keys [@c, 'a'], [@c, 'x']
-      receiver.value.should be_empty
-      events.should == 7
-    end
-
-    it 'supports combination of strings and arrays' do
-      receiver.send_keys 'foo', [@c, 'a'], :backspace
-      receiver.value.should be_empty
-      events.should == 6
-    end
-  end
-
   describe '#present?' do
     before do
       browser.goto('file://' + File.expand_path('html/wait.html', File.dirname(__FILE__)))
