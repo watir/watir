4b5d3f9dc878a8cca48a3f18a44b4695da0d6820
Jari Bakken <jari.bakken@gmail.com>
2011-09-25 23:07:15 +0200

    Nasty hack to work around http://redmine.ruby-lang.org/issues/5218.

--- spec/watirspec/lib/watirspec.rb
+++ spec/watirspec/lib/watirspec.rb
@@ -1,4 +1,19 @@
 # encoding: utf-8
+
+# see http://redmine.ruby-lang.org/issues/5218
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+  module Kernel
+    alias :__at_exit :at_exit
+    def at_exit(&block)
+      __at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        block.call
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
+
 module WatirSpec
   class << self
     attr_accessor :browser_args, :persistent_browser, :unguarded, :implementation
