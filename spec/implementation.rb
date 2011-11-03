9654722e99f0ed4e7c6a8d25a3da0e8ae2a21b53
Jari Bakken <jari.bakken@gmail.com>
2011-11-03 21:27:05 +0100

    Using Travis to check how things run with native events enabled.

--- spec/implementation.rb
+++ spec/implementation.rb
@@ -6,7 +6,17 @@ WatirSpec.implementation do |imp|
 
   imp.name          = name
   imp.browser_class = Watir::Browser
-  imp.browser_args  = [browser]
+
+  if browser == :firefox && ENV['NATIVE_EVENTS'] == "true"
+    profile = Selenium::WebDriver::Firefox::Profile.new
+    profile.native_events = true
+
+    imp.browser_args = [:firefox, :profile => profile]
+  elsif browser == :chrome && ENV['NATIVE_EVENTS'] == "true"
+    imp.browser_args = [:chrome, :native_events => true]
+  else
+    imp.browser_args = [browser]
+  end
 
   imp.guard_proc = lambda { |args|
     args.any? { |arg| arg == name || arg == browser || arg == [name, browser]}
