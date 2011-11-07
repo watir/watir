304cd71efaf44ff76cd57565b11c9bb65ae49386
Jari Bakken <jari.bakken@gmail.com>
2011-11-07 18:19:11 +0100

    Add specs for window resize/move available in Firefox + Webdriver >= 2.12.0

--- spec/watirspec/window_switching_spec.rb
+++ spec/watirspec/window_switching_spec.rb
@@ -46,7 +46,7 @@ describe "Browser" do
       w = browser.window(:title => "closeable window").use
       w.should be_kind_of(Window)
     end
-    
+
     it "finds window by :index" do
       w = browser.window(:index => 1).use
       w.should be_kind_of(Window)
@@ -75,7 +75,7 @@ describe "Browser" do
     it "raises an error if no window matches the selector" do
       lambda { browser.window(:title => "noop").use }.should raise_error
     end
-    
+
     it "raises an error if there's no window at the given index" do
       lambda { browser.window(:index => 100).use }.should raise_error
     end
@@ -167,7 +167,7 @@ bug "http://github.com/jarib/celerity/issues#issue/17", :celerity do
         win1.should_not == win2
       end
     end
-    
+
     describe "#when_present" do
       it "waits until the window is present" do
         # TODO: improve this spec.
@@ -175,14 +175,58 @@ bug "http://github.com/jarib/celerity/issues#issue/17", :celerity do
         browser.window(:title => "closeable window").when_present do
           did_yield = true
         end
-        
+
         did_yield.should be_true
       end
-      
+
       it "times out waiting for a non-present window" do
-        lambda { 
+        lambda {
           browser.window(:title => "noop").wait_until_present(0.5)
         }.should raise_error(Wait::TimeoutError, 'timed out after 0.5 seconds, waiting for {:title=>"noop"} to become present')
+      end
+    end
+
+    describe "size and position" do
+      compliant_on [:webdriver, :firefox] do
+        it "should get the size of the current window" do
+          size = browser.window.size
+
+          size.width.should > 0
+          size.height.should > 0
+        end
+
+        it "should get the position of the current window" do
+          pos = browser.window.position
+
+          pos.x.should >= 0
+          pos.y.should >= 0
+        end
+
+        it "should resize the window" do
+          initial_size = browser.window.size
+          browser.window.resize_to(
+            initial_size.width - 10,
+            initial_size.height - 10
+          )
+
+          new_size = browser.window.size
+
+          new_size.width.should == initial_size.width - 10
+          new_size.height.should == initial_size.height - 10
+        end
+
+        it "should move the window" do
+          initial_pos = browser.window.position
+
+          browser.window.move_to(
+            initial_pos.x + 10,
+            initial_pos.y + 10
+          )
+
+          new_pos = browser.window.position
+          new_pos.x.should == initial_pos.x + 10
+          new_pos.y.should == initial_pos.y + 10
+        end
       end
     end
   end
