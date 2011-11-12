1b664af685fd3fc2b0f07edfd106e04a2b956453
Jari Bakken <jari.bakken@gmail.com>
2011-11-12 16:56:52 +0100

    Update to a newer build of chromedriver on travis.

--- support/travis.sh
+++ support/travis.sh
@@ -4,11 +4,11 @@ sh -e /etc/init.d/xvfb start && git submodule update --init || exit 1
 
 if [[ "$WATIR_WEBDRIVER_BROWSER" = "chrome" ]]; then
   curl -L https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-  sudo -s 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list'
+  sudo -s 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
   sudo apt-get update
   sudo apt-get -y install google-chrome-unstable unzip
   # curl -k http://chromium.googlecode.com/files/chromedriver_linux32_14.0.836.0.zip | sudo -s 'funzip > /usr/bin/chromedriver'
-  sudo -s 'curl -L http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/102682/chrome-linux.test/chromedriver > /usr/bin/chromedriver'
+  sudo -s 'curl -L http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/109799/chrome-linux.test/chromedriver > /usr/bin/chromedriver'
   sudo chmod +x /usr/bin/chromedriver
 fi
 
