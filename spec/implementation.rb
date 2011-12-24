50b3782fc04ab0cc6b7ef7ea322a721cc65ae09d
Jari Bakken <jari.bakken@gmail.com>
2011-12-24 01:34:06 +0100

    Add the ability to differentiate native vs synthesized events when running specs.

--- spec/implementation.rb
+++ spec/implementation.rb
@@ -1,25 +1,40 @@
 require File.expand_path("../spec_helper", __FILE__)
 
 WatirSpec.implementation do |imp|
-  name    = :webdriver
   browser = (ENV['WATIR_WEBDRIVER_BROWSER'] || :firefox).to_sym
 
-  imp.name          = name
+  imp.name          = :webdriver
   imp.browser_class = Watir::Browser
 
-  if browser == :firefox && ENV['NATIVE_EVENTS'] == "true"
+  use_native_events = ENV['NATIVE_EVENTS'] == "true"
+
+  if browser == :firefox && use_native_events
     profile = Selenium::WebDriver::Firefox::Profile.new
     profile.native_events = true
 
     imp.browser_args = [:firefox, {:profile => profile}]
-  elsif browser == :chrome && ENV['NATIVE_EVENTS'] == "true"
+  elsif browser == :chrome && use_native_events
     imp.browser_args = [:chrome, {:native_events => true}]
   else
     imp.browser_args = [browser]
   end
 
+  matching_guards = [
+    :webdriver,            # guard only applies to webdriver
+    browser,               # guard only applies to this browser
+    [:webdriver, browser]  # guard only applies to this browser on webdriver
+  ]
+
+  if use_native_events || (Selenium::WebDriver::Platform.windows? && [:firefox, :ie].include?(browser))
+    # guard only applies to this browser on webdriver with native events enabled
+    matching_guards << [:webdriver, browser, :native_events]
+  else
+    # guard only applies to this browser on webdriver with native events disabled
+    matching_guards << [:webdriver, browser, :synthesized_events]
+  end
+
   imp.guard_proc = lambda { |args|
-    args.any? { |arg| arg == name || arg == browser || arg == [name, browser]}
+    args.any? { |arg| matching_guards.include?(arg) }
   }
 end
 
