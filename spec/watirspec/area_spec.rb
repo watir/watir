6411c4d9f3d9dfc4c101e088ba02605140ed21d2
Jarmo Pertman <jarmo.p@gmail.com>
2011-12-30 13:51:30 +0200

    modify guard for IE

--- spec/watirspec/area_spec.rb
+++ spec/watirspec/area_spec.rb
@@ -15,7 +15,7 @@ describe "Area" do
       browser.area(:title, "Tables").should exist
       browser.area(:title, /Tables/).should exist
 
-      not_compliant_on [:webdriver, :ie], :watir do
+      not_compliant_on :ie do
         browser.area(:href, "tables.html").should exist
       end
 
