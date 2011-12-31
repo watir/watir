9307396144da1b6a13312f535dadee4e398e6ca0
Jari Bakken <jari.bakken@gmail.com>
2011-12-31 17:08:26 +0100

    Fix typo.

--- support/travis.sh
+++ support/travis.sh
@@ -9,6 +9,6 @@ if [[ "$WATIR_WEBDRIVER_BROWSER" = "chrome" ]]; then
   curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/116062/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
   ln -s chrome-linux/chrome chrome-linux/google-chrome
-  export PATH="${pwd}/chrome-linux":$PATH
+  export PATH=`pwd`/chrome-linux:$PATH
 fi
 
