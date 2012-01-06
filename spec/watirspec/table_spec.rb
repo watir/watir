5aaf242b77a6ec83a08f9f8d2944306708fc6f61
Jarmo Pertman <jarmo.p@gmail.com>
2012-01-06 15:34:23 +0200

    add guard for Watir

--- spec/watirspec/table_spec.rb
+++ spec/watirspec/table_spec.rb
@@ -49,11 +49,21 @@ describe "Table" do
          "Table 2, Row 1, Cell 2"]
       ]
 
-      browser.table(:id, 'outer').strings.should == [
-        ["Table 1, Row 1, Cell 1", "Table 1, Row 1, Cell 2"],
-        ["Table 1, Row 2, Cell 1", "Table 1, Row 2, Cell 2\nTable 2, Row 1, Cell 1 Table 2, Row 1, Cell 2"],
-        ["Table 1, Row 3, Cell 1", "Table 1, Row 3, Cell 2"]
-     ]
+      not_compliant_on :watir do
+        browser.table(:id, 'outer').strings.should == [
+          ["Table 1, Row 1, Cell 1", "Table 1, Row 1, Cell 2"],
+          ["Table 1, Row 2, Cell 1", "Table 1, Row 2, Cell 2\nTable 2, Row 1, Cell 1 Table 2, Row 1, Cell 2"],
+          ["Table 1, Row 3, Cell 1", "Table 1, Row 3, Cell 2"]
+       ]
+      end
+
+      deviates_on :watir do
+        browser.table(:id, 'outer').strings.should == [
+          ["Table 1, Row 1, Cell 1", "Table 1, Row 1, Cell 2"],
+          ["Table 1, Row 2, Cell 1", "Table 1, Row 2, Cell 2 Table 2, Row 1, Cell 1 Table 2, Row 1, Cell 2"],
+          ["Table 1, Row 3, Cell 1", "Table 1, Row 3, Cell 2"]
+       ]
+      end
     end
   end
 
