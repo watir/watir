62a2247c2dc967aac78dc77e8f19dd3c73f1ac83
Jari Bakken <jari.bakken@gmail.com>
2012-01-16 23:26:46 +0100

    Bump travis Chrome rev

--- support/travis.sh
+++ support/travis.sh
@@ -1,12 +1,13 @@
 #/bin/sh
 
+CHROME_REVISION=117862
 sh -e /etc/init.d/xvfb start && git submodule update --init || exit 1
 
 if [[ "$WATIR_WEBDRIVER_BROWSER" = "chrome" ]]; then
   sudo apt-get install -y unzip libxss1
-  curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/116062/chrome-linux.zip"
+  curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.zip"
   unzip chrome-linux.zip
-  curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/116062/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
+  curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
   ln -s chrome-linux/chrome chrome-linux/google-chrome
 fi
