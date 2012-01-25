cd827758fbe978c7e55b2c6fbebc108c04027486
Jari Bakken <jari.bakken@gmail.com>
2012-01-25 22:59:02 +0100

    Add spec for #textarea

---
+++ spec/watirspec/textarea_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path("../spec_helper", __FILE__)
+
+describe Watir::TextArea do
+  before :each do
+    browser.goto File.join(WatirSpec.files, 'forms_with_input_elements.html')
+  end
+
+  let(:textarea) { browser.textarea }
+
+  it 'can set a value' do
+    textarea.set 'foo'
+    textarea.value.should == 'foo'
+  end
+
+  it 'can clear a value' do
+    textarea.set 'foo'
+    textarea.clear
+    textarea.value.should == ''
+  end
+
+end
