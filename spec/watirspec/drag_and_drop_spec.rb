6ab7094af5e810430bc7c6f2adedd473950895d8
Jari Bakken <jari.bakken@gmail.com>
2012-01-31 01:03:42 +0100

    Add spec for Element#drag_and_drop_{on,by} based on jQuery UI demo.
    See watir/watir-webdriver#75

---
+++ spec/watirspec/drag_and_drop_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path("../spec_helper", __FILE__)
+
+describe "Element" do
+  context "drag and drop" do
+    before { browser.goto WatirSpec.files + "/drag_and_drop.html" }
+
+    let(:draggable) { browser.div :id => "draggable" }
+    let(:droppable) { browser.div :id => "droppable" }
+
+    it "can drag and drop an element onto another" do
+      droppable.text.should == 'Drop here'
+      draggable.drag_and_drop_on droppable
+      droppable.text.should == 'Dropped!'
+    end
+
+    it "can drag an element by the given offset" do
+      droppable.text.should == 'Drop here'
+      draggable.drag_and_drop_by 200, 50
+      droppable.text.should == 'Dropped!'
+    end
+  end
+end
