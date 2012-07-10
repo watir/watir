3b62b0d0314aefd3cf2dcba73e0d6e801a0d32fe
Alex Rodionov <p0deje@gmail.com>
2012-07-10 11:39:08 +0700

    maximize works only on firefox

--- spec/watirspec/window_switching_spec.rb
+++ spec/watirspec/window_switching_spec.rb
@@ -225,15 +225,17 @@ not_compliant_on [:webdriver, :iphone], [:webdriver, :safari] do
           new_pos.y.should == initial_pos.y + 10
         end
 
-        it "should maximize the window" do
-          initial_size = browser.window.size
+        compliant_on [:webdriver, :firefox] do
+          it "should maximize the window" do
+            initial_size = browser.window.size
 
-          browser.window.maximize
+            browser.window.maximize
 
-          browser.wait_until { browser.window.size != initial_size }
-          new_size = browser.window.size
-          new_size.width.should > initial_size.width
-          new_size.height.should > initial_size.height
+            browser.wait_until { browser.window.size != initial_size }
+            new_size = browser.window.size
+            new_size.width.should > initial_size.width
+            new_size.height.should > initial_size.height
+          end
         end
       end
     end
