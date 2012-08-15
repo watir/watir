7f2208a9ccc89bd813fb805333babffb06037bd3
Jarmo Pertman <jarmo.p@gmail.com>
2012-08-15 09:30:15 +0300

    Improve screenshot_spec to not be Watir-WebDriver specific.

--- spec/watirspec/screenshot_spec.rb
+++ spec/watirspec/screenshot_spec.rb
@@ -1,25 +1,27 @@
-require File.expand_path("watirspec/spec_helper", File.dirname(__FILE__))
+require "base64"
+require File.expand_path("../spec_helper", __FILE__)
 
-describe Watir::Screenshot do
+describe "Watir::Screenshot" do
   describe '#png' do
-    it 'gets png representation of screenshot from WebDriver' do
-      browser.driver.should_receive(:screenshot_as).with(:png)
-      browser.screenshot.png
+    it 'gets png representation of screenshot' do
+      browser.screenshot.png[0..3].should == "\211PNG"
     end
   end
 
   describe '#base64' do
-    it 'gets base64 representation of screenshot from WebDriver' do
-      browser.driver.should_receive(:screenshot_as).with(:base64)
-      browser.screenshot.base64
+    it 'gets base64 representation of screenshot' do
+      image = browser.screenshot.base64
+      Base64.decode64(image)[0..3].should == "\211PNG"
     end
   end
 
   describe '#save' do
     it 'saves screenshot to given file' do
-      path = "#{Dir.tmpdir}/test.png"
-      browser.driver.should_receive(:save_screenshot).with(path)
+      path = "#{Dir.tmpdir}/test#{Time.now.to_i}.png"
+      File.should_not exist(path)
       browser.screenshot.save(path)
+      File.should exist(path)
+      File.open(path, "rb") {|io| io.read}[0..3].should == "\211PNG"
     end
   end
 end
