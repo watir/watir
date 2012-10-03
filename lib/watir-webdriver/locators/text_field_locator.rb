8e323c28e799b44eeff5d30c68af077503ea60f7
Alex Rodionov <p0deje@gmail.com>
2012-10-04 01:22:35 +0700

    fix bug with substring text_field locator; however now locator is unbeliavable long

--- lib/watir-webdriver/locators/text_field_locator.rb
+++ lib/watir-webdriver/locators/text_field_locator.rb
@@ -3,7 +3,7 @@ module Watir
 
     NON_TEXT_TYPES     = %w[file radio checkbox submit reset image button hidden datetime date month week time datetime-local range color]
     # TODO: better way of finding input text fields?
-    NEGATIVE_TYPE_EXPR = "contains('%s', %s)=false" % [NON_TEXT_TYPES.map { |t| t }.join(","), XpathSupport.downcase('@type')]
+    NEGATIVE_TYPE_EXPR = NON_TEXT_TYPES.map { |type| "%s!='%s'" % [XpathSupport.downcase('@type'), type] }.join(' and ')
 
     def locate_all
       find_all_by_multiple
