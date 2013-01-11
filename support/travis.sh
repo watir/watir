6ae78189d9bb5d4faf52f273d27323a85b848337
Jarmo Pertman <jarmo.p@gmail.com>
2013-01-11 18:34:48 +0200

    Run specs against PhantomJS too. Bump WatirSpec.

--- support/travis.sh
+++ support/travis.sh
@@ -13,3 +13,11 @@ if [[ "$WATIR_WEBDRIVER_BROWSER" = "chrome" ]]; then
   curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
 fi
+
+if [[ "$WATIR_WEBDRIVER_BROWSER" = "phantomjs" ]]; then
+  curl -L -O "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-i686.tar.bz2"
+  bzip2 -cd phantomjs-1.8.1-linux-i686.tar.bz2 | tar xvf -
+  chmod +x phantomjs-1.8.1-linux-i686/bin/phantomjs
+  sudo cp phantomjs-1.8.1-linux-i686/bin/phantomjs /usr/local/phantomjs/bin/phantomjs
+  phantomjs --version
+fi
