44cde9808aa9cabe32d1693e260f8263a61f03e7
Jari Bakken <jari.bakken@gmail.com>
2013-01-12 10:31:30 +0100

    Fix for Ruby 2.0 (default UTF-8 encoding)

--- spec/watirspec/screenshot_spec.rb
+++ spec/watirspec/screenshot_spec.rb
@@ -2,16 +2,23 @@ require "base64"
 require File.expand_path("../spec_helper", __FILE__)
 
 describe "Watir::Screenshot" do
+  let(:png_header) do
+    str = "\211PNG"
+    str.force_encoding('BINARY') if str.respond_to?(:force_encoding)
+
+    str
+  end
+
   describe '#png' do
     it 'gets png representation of screenshot' do
-      browser.screenshot.png[0..3].should == "\211PNG"
+      browser.screenshot.png[0..3].should == png_header
     end
   end
 
   describe '#base64' do
     it 'gets base64 representation of screenshot' do
       image = browser.screenshot.base64
-      Base64.decode64(image)[0..3].should == "\211PNG"
+      Base64.decode64(image)[0..3].should == png_header
     end
   end
 
@@ -21,7 +28,7 @@ describe "Watir::Screenshot" do
       File.should_not exist(path)
       browser.screenshot.save(path)
       File.should exist(path)
-      File.open(path, "rb") {|io| io.read}[0..3].should == "\211PNG"
+      File.open(path, "rb") {|io| io.read}[0..3].should == png_header
     end
   end
 end
