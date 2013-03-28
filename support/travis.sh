4be4a46a52abaf8a4e21b960f0d080c5276bac75
Jari Bakken <jari.bakken@gmail.com>
2013-03-28 18:17:59 +0100

    Travis: Download the 64-bit version of Chrome.

--- support/travis.sh
+++ support/travis.sh
@@ -8,9 +8,9 @@ sh -e /etc/init.d/xvfb start && git submodule update --init
 
 if [[ "$WATIR_WEBDRIVER_BROWSER" = "chrome" ]]; then
   sudo apt-get install -y unzip libxss1
-  curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.zip"
+  curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.zip"
   unzip chrome-linux.zip
-  curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
+  curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
 fi
 
