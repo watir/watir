934ff93d8f904300b9690690bc5bbeeefd578531
Jari Bakken <jari.bakken@gmail.com>
2013-03-28 20:42:31 +0100

    Latest chrome has issues with window resizing - guarding the specs for now.

--- spec/watirspec/window_switching_spec.rb
+++ spec/watirspec/window_switching_spec.rb
@@ -198,7 +198,9 @@ not_compliant_on [:webdriver, :iphone], [:webdriver, :safari] do
           pos.x.should >= 0
           pos.y.should >= 0
         end
+      end
 
+      compliant_on [:webdriver, :firefox] do
         it "should resize the window" do
           initial_size = browser.window.size
           browser.window.resize_to(
@@ -224,18 +226,18 @@ not_compliant_on [:webdriver, :iphone], [:webdriver, :safari] do
           new_pos.x.should == initial_pos.x + 10
           new_pos.y.should == initial_pos.y + 10
         end
+      end
 
-        compliant_on [:webdriver, :firefox, :window_manager] do
-          it "should maximize the window" do
-            initial_size = browser.window.size
+      compliant_on [:webdriver, :firefox, :window_manager] do
+        it "should maximize the window" do
+          initial_size = browser.window.size
 
-            browser.window.maximize
+          browser.window.maximize
+          browser.wait_until { browser.window.size != initial_size }
 
-            browser.wait_until { browser.window.size != initial_size }
-            new_size = browser.window.size
-            new_size.width.should > initial_size.width
-            new_size.height.should > initial_size.height
-          end
+          new_size = browser.window.size
+          new_size.width.should > initial_size.width
+          new_size.height.should > initial_size.height
         end
       end
     end
