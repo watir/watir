3d2be86bec04ebb510f34b9ec32eb49086239d86
Titus Fortner <titus.fortner@blackbaud.com>
2014-10-21 16:48:20 -0500

    specs for error checkers

--- spec/watirspec/browser_spec.rb
+++ spec/watirspec/browser_spec.rb
@@ -322,6 +322,96 @@ describe "Browser" do
     end
   end
 
+  describe "#run_checkers" do
+    after(:each) do
+      browser.window(:index, 0).use
+      browser.alert.close if browser.alert.exists?
+      browser.disable_checker @page_checker
+    end
+
+    it "runs checkers after Browser#goto" do
+      @page_checker = Proc.new { raise if browser.title == "The font element" }
+      browser.add_checker @page_checker
+      expect { browser.goto WatirSpec.url_for("font.html") }.to raise_error
+    end
+
+    it "runs checkers after Browser#refresh" do
+      browser.goto WatirSpec.url_for("font.html")
+      @page_checker = Proc.new { raise if browser.title == "The font element" }
+      browser.add_checker @page_checker
+      expect { browser.refresh }.to raise_error
+    end
+
+    it "runs checkers after Alert#ok" do
+      browser.goto WatirSpec.url_for("alerts.html")
+      @page_checker = Proc.new { raise if browser.title == "Alerts" }
+      browser.add_checker @page_checker
+      expect { browser.alert.ok }.to raise_error
+    end
+
+    it "runs checkers after Element#click" do
+      browser.goto(WatirSpec.url_for("non_control_elements.html"))
+      @page_checker = Proc.new { raise if browser.title == "Non-control elements" }
+      browser.add_checker @page_checker
+      expect { browser.link(:index, 0).click }.to raise_error
+    end
+
+    not_compliant_on [:webdriver, :iphone] do
+      it "runs checkers after Element#double_click" do
+        browser.goto(WatirSpec.url_for("non_control_elements.html"))
+        @page_checker = Proc.new { raise if browser.title == "Non-control elements" }
+        browser.add_checker @page_checker
+        expect { browser.div(:id, 'html_test').double_click }.to raise_error
+      end
+    end
+
+    it "runs checkers after Element#right_click" do
+      browser.goto(WatirSpec.url_for("right_click.html"))
+      @page_checker = Proc.new { raise if browser.title == "Right Click Test" }
+      browser.add_checker @page_checker
+      expect { browser.div(:id, "click").right_click }.to raise_error
+    end
+
+    it "raises UnhandledAlertError error when running error checks with alert present" do
+      url = WatirSpec.url_for("alerts.html")
+      @page_checker = Proc.new { browser.url }
+      browser.add_checker @page_checker
+      browser.goto url
+      expect { browser.button(:id => "alert").click }.to raise_error(Selenium::WebDriver::Error::UnhandledAlertError)
+    end
+
+    it "does not raise error when running error checks using #without_checkers with alert present" do
+      url = WatirSpec.url_for("alerts.html")
+      @page_checker = Proc.new { browser.url }
+      browser.add_checker @page_checker
+      browser.goto url
+      expect { browser.without_checkers {browser.button(:id => "alert").click} }.to_not raise_error
+    end
+
+    it "does not raise error if no error checks are defined with alert present" do
+      url = WatirSpec.url_for("alerts.html")
+      @page_checker = Proc.new { browser.url }
+      browser.add_checker @page_checker
+      browser.goto url
+      browser.disable_checker @page_checker
+      expect { browser.button(:id => "alert").click }.to_not raise_error
+      browser.alert.close
+    end
+
+    it "does not raise error when running error checks on closed window" do
+      url = WatirSpec.url_for("window_switching.html")
+      @page_checker = Proc.new { browser.url }
+      browser.add_checker @page_checker
+      browser.goto url
+      browser.a(:id => "open").click
+
+      window = browser.window(:title => "closeable window")
+      window.use
+      expect { browser.a(:id => "close").click }.to_not raise_error
+      browser.window(:index, 0).use
+    end
+  end
+
   it "raises UnknownObjectException when trying to access DOM elements on plain/text-page" do
     browser.goto(WatirSpec.url_for("plain_text", :needs_server => true))
     expect { browser.div(:id, 'foo').id }.to raise_error(UnknownObjectException)
