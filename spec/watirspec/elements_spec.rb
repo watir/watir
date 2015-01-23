2c3d9483f109b74c99310e207b7af900d90f448d
Alex Rodionov <p0deje@gmail.com>
2015-01-23 14:37:54 +0600

    Element collection for non-existing locator should return non-existing element

---
+++ spec/watirspec/elements_spec.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+require File.expand_path("../spec_helper", __FILE__)
+
+describe "Elements" do
+  before :each do
+    browser.goto(WatirSpec.url_for("forms_with_input_elements.html"))
+  end
+
+  describe "#[]" do
+    context "when elements do not exist" do
+      it "returns not existing element" do
+        expect(browser.elements(:id => "non-existing")[0]).not_to exist
+      end
+    end
+  end
+end
