e5537477c694cc0ed4d7d104666e6c42b734736c
Titus Fortner <titusfortner@gmail.com>
2015-10-04 12:07:54 -0500

    Native Events no longer supported in Firefox

--- spec/click_spec.rb
+++ spec/click_spec.rb
@@ -15,29 +15,5 @@ describe Watir::Element do
         expect(browser.element(text: "You Clicked It!")).to exist
       end
     end
-
-    # TODO: make guards more flexible, in reality this currently only works on linux with native events
-    compliant_on %i(webdriver firefox native_events) do
-      it "should perform a click with no modifier keys" do
-        clicker.click
-        expect(log).to eq ["shift=false alt=false"]
-      end
-
-      it "should perform a click with the shift key pressed" do
-        clicker.click(:shift)
-        expect(log).to eq ["shift=true alt=false"]
-      end
-
-      it "should perform a click with the alt key pressed" do
-        clicker.click(:alt)
-        expect(log).to eq ["shift=false alt=true"]
-      end
-
-      it "should perform a click with the shift and alt keys pressed" do
-        clicker.click(:shift, :alt)
-        expect(log).to eq ["shift=true alt=true"]
-      end
-    end
-
   end
 end
