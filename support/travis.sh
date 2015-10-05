046f72891993440ed76248b601244b693bc0ced2
Alex Rodionov <p0deje@gmail.com>
2015-10-05 12:32:16 +0600

    Use Travis-bundled PhantomJS

--- support/travis.sh
+++ support/travis.sh
@@ -32,12 +32,3 @@ if [[ "$WATIR_WEBDRIVER_BROWSER" = "chrome" ]]; then
   mv chromedriver chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
 fi
-
-if [[ "$WATIR_WEBDRIVER_BROWSER" = "phantomjs" ]]; then
-  PHANTOMJS_NAME=phantomjs-1.9.0-linux-x86_64
-  curl -L -O "https://phantomjs.googlecode.com/files/$PHANTOMJS_NAME.tar.bz2"
-  tar -xvjf $PHANTOMJS_NAME.tar.bz2
-  chmod +x $PHANTOMJS_NAME/bin/phantomjs
-  sudo cp $PHANTOMJS_NAME/bin/phantomjs /usr/local/phantomjs/bin/phantomjs
-  phantomjs --version
-fi
