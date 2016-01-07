b6563c6f86e26eb8b0d8adf9cf46c5969db79070
Alex Rodionov <p0deje@gmail.com>
2016-01-07 14:34:05 +0600

    Split ElementLocator into smaller nested classes
    An attempt to identify the responsibilties

--- lib/watir-webdriver/elements/element.rb
+++ lib/watir-webdriver/elements/element.rb
@@ -608,7 +608,7 @@ module Watir
 
     def method_missing(meth, *args, &blk)
       method = meth.to_s
-      if method =~ ElementLocator::WILDCARD_ATTRIBUTE
+      if method =~ ElementLocator::SelectorBuild::WILDCARD_ATTRIBUTE
         attribute_value(method.gsub(/_/, '-'), *args)
       else
         super
@@ -616,7 +616,7 @@ module Watir
     end
 
     def respond_to_missing?(meth, *)
-      ElementLocator::WILDCARD_ATTRIBUTE === meth.to_s || super
+      ElementLocator::SelectorBuild::WILDCARD_ATTRIBUTE === meth.to_s || super
     end
 
   end # Element
