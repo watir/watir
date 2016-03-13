618b0879c903688f7fe30ee4adc0cf63d317ea5a
Titus Fortner <titus.fortner@gmail.com>
2016-03-13 12:59:33 -0500

    update to latest phantomjs version

--- support/travis.sh
+++ support/travis.sh
@@ -22,3 +22,12 @@ if [[ "$RAKE_TASK" = "spec:chrome" ]]; then
   mv chromedriver chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
 fi
+
+if [[ "$WATIR_WEBDRIVER_BROWSER" = "phantomjs" ]]; then
+  PHANTOMJS_NAME=phantomjs-2.1.1-linux-x86_64
+  curl -L -O "https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOMJS_NAME}.tar.bz2"
+  tar -xvjf $PHANTOMJS_NAME.tar.bz2
+  chmod +x $PHANTOMJS_NAME/bin/phantomjs
+  sudo cp $PHANTOMJS_NAME/bin/phantomjs /usr/local/phantomjs/bin/phantomjs
+  phantomjs --version
+fi
