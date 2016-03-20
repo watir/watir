70f9aed7f3ea380764dd945cb5f6fb64dc5959e1
Titus Fortner <titus.fortner@gmail.com>
2016-03-19 21:28:30 -0500

    travis-ci move phantomjs to path without sudo

--- support/travis.sh
+++ support/travis.sh
@@ -28,6 +28,6 @@ if [[ "$RAKE_TASK" = "spec:phantomjs" ]]; then
   curl -L -O "https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOMJS_NAME}.tar.bz2"
   tar -xvjf $PHANTOMJS_NAME.tar.bz2
   chmod +x $PHANTOMJS_NAME/bin/phantomjs
-  sudo cp $PHANTOMJS_NAME/bin/phantomjs /usr/local/phantomjs/bin/phantomjs
+  mv $PHANTOMJS_NAME/bin/phantomjs chrome-linux/phantomjs
   phantomjs --version
 fi
