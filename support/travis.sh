3c0323618c21c146b0b3e4782659d81017944179
Alex Rodionov <p0deje@gmail.com>
2016-07-22 10:07:35 +0800

    Lock Chrome version to 51

--- support/travis.sh
+++ support/travis.sh
@@ -12,7 +12,9 @@ if [[ "$RAKE_TASK" = "yard:doctest" ]]; then
 fi
 
 if [[ "$RAKE_TASK" = "spec:chrome" ]]; then
-  CHROME_REVISION=$(curl -s http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/LAST_CHANGE)
+  # https://omahaproxy.appspot.com
+  # https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/
+  CHROME_REVISION=386257
   curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROME_REVISION}/chrome-linux.zip"
   unzip chrome-linux.zip
 
