8af1fcb1163210521d3fd78f0549a0a8b9ec1440
Alex Rodionov <p0deje@gmail.com>
2016-08-07 10:39:51 +0800

    Use :phantomjs for Watir::Browser#name doctest
    When using :chrome on Travis, we need to pass --no-sandbox argument, so
    either doctest should be different or we can just use other driver

--- lib/watir/browser.rb
+++ lib/watir/browser.rb
@@ -152,9 +152,9 @@ module Watir
     # Returns browser name.
     #
     # @example
-    #   browser = Watir::Browser.new :chrome
+    #   browser = Watir::Browser.new :phantomjs
     #   browser.name
-    #   #=> :chrome
+    #   #=> :phantomjs
     #
     # @return [Symbol]
     #
