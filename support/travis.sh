d13f470f9afab36df60d7e510d9e2ec72f9cfd7c
Titus Fortner <titusfortner@gmail.com>
2016-09-10 14:59:09 -0500

    lock in chrome 55 snapshot

--- support/travis.sh
+++ support/travis.sh
@@ -14,7 +14,9 @@ fi
 if [[ "$RAKE_TASK" = "spec:chrome" ]] || [[ "$RAKE_TASK" = "yard:doctest" ]]; then
   # https://omahaproxy.appspot.com
   # https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/
-  CHROME_REVISION=`curl -s http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/LAST_CHANGE`
+  # CHROME_REVISION=`curl -s http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/LAST_CHANGE`
+  CHROME_REVISION=417841
+
   curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROME_REVISION}/chrome-linux.zip"
   unzip chrome-linux.zip
 
