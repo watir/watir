368589818d10e70cc73e17fa11853423ddc6c4a0
Alex Rodionov <p0deje@gmail.com>
2016-10-13 13:21:13 +0600

    Properly handle content-types for different static files

--- lib/watirspec/server/app.rb
+++ lib/watirspec/server/app.rb
@@ -11,6 +11,14 @@ module WatirSpec
           respond('This is text/plain', 'Content-Type' => 'text/plain')
         when %r{/set_cookie}
           respond("<html>C is for cookie, it's good enough for me</html>", 'Content-Type' => 'text/html', 'Set-Cookie' => 'monster=1')
+        when css_file?
+          respond(File.read("#{WatirSpec.html}/#{path}"), 'Content-Type' => 'text/css')
+        when js_file?
+          respond(File.read("#{WatirSpec.html}/#{path}"), 'Content-Type' => 'application/javascript')
+        when png_file?
+          respond(File.binread("#{WatirSpec.html}/#{path}"), 'Content-Type' => 'image/png')
+        when gif_file?
+          respond(File.read("#{WatirSpec.html}/#{path}"), 'Content-Type' => 'image/gif')
         when static_file?
           respond(File.read("#{WatirSpec.html}/#{path}"))
         else
@@ -22,6 +30,22 @@ module WatirSpec
 
       def respond(body, headers = {}, status = '200 OK')
         [status, headers, body]
+      end
+
+      def css_file?
+        proc { |path| static_files.include?(path) && path.end_with?('.css') }
+      end
+
+      def js_file?
+        proc { |path| static_files.include?(path) && path.end_with?('.js') }
+      end
+
+      def png_file?
+        proc { |path| static_files.include?(path) && path.end_with?('.png') }
+      end
+
+      def gif_file?
+        proc { |path| static_files.include?(path) && path.end_with?('.gif') }
       end
 
       def static_file?
