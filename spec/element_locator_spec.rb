42390141a5fde78319acd89a42e9f7bdc3af98f7
Titus Fortner <titusfortner@gmail.com>
2017-01-01 11:39:02 -0600

    fix syntax issue

--- spec/element_locator_spec.rb
+++ spec/element_locator_spec.rb
@@ -289,9 +289,9 @@ describe Watir::Locators::Element::Locator do
       end
 
       it "raises a TypeError if selector value is not a String or Regexp" do
-         num_type = RUBY_VERSION[/^\d+\.(\d+)/, 1].to_i >= 4 ? 'Integer' : 'Fixnum'
-          expect { locate_one(tag_name: 123) }.to \
- +        raise_error(TypeError, %[expected one of [String, Regexp], got 123:#{num_type}])
+        num_type = RUBY_VERSION[/^\d+\.(\d+)/, 1].to_i >= 4 ? 'Integer' : 'Fixnum'
+        expect { locate_one(tag_name: 123) }.to \
+        raise_error(TypeError, %[expected one of [String, Regexp], got 123:#{num_type}])
       end
 
       it "raises a MissingWayOfFindingObjectException if the attribute is not valid" do
