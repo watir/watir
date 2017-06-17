9c738472519cec22a787bb1c1dcf599094876fab
Titus Fortner <titusfortner@gmail.com>
2017-06-17 15:31:58 -0700

    use webdrivers gem

--- support/travis.sh
+++ support/travis.sh
@@ -9,29 +9,3 @@ if [[ "$RAKE_TASK" = "yard:doctest" ]]; then
   mkdir ~/.yard
   bundle exec yard config -a autoload_plugins yard-doctest
 fi
-
-if [[ "$RAKE_TASK" = "spec:chrome" ]] || [[ "$RAKE_TASK" = "yard:doctest" ]]; then
-  CHROMEDRIVER_VERSION=$(curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-  curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
-  unzip chromedriver_linux64.zip
-
-  mv chromedriver travis-drivers/chromedriver
-  chmod +x travis-drivers/chromedriver
-fi
-
-if [[ "$RAKE_TASK" = "spec:firefox" ]]; then
-  curl -L -O "http://raw.githubusercontent.com/watir/driver_binaries/master/geckodriver"
-  chmod +x geckodriver
-
-  mv geckodriver travis-drivers/geckodriver
-  geckodriver --version
-  firefox --version
-fi
-
-if [[ "$RAKE_TASK" = "spec:phantomjs" ]]; then
-  curl -L -O "http://raw.githubusercontent.com/watir/driver_binaries/master/phantomjs"
-  chmod +x phantomjs
-
-  mv phantomjs travis-drivers/phantomjs
-  phantomjs --version
-fi
--- watir.gemspec
+++ watir.gemspec
@@ -33,4 +33,5 @@ It facilitates the writing of automated tests by mimicing the behavior of a user
   s.add_development_dependency 'pry'
   s.add_development_dependency 'coveralls'
   s.add_development_dependency 'yard-doctest', '>= 0.1.8'
+  s.add_development_dependency 'webdrivers'
 end
