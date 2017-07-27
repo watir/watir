eecb5d3bf2975f4959b27f8f1468848b42a5d38c
Titus Fortner <titusfortner@gmail.com>
2017-07-27 13:08:22 -0500

    syntax fixes

--- spec/watirspec/html/javascript/helpers.js
+++ spec/watirspec/html/javascript/helpers.js
@@ -2,11 +2,11 @@ var WatirSpec = function() {
     
   return {
     addMessage: function(string) {
-        var text = document.createTextNode(string)
+        var text = document.createTextNode(string);
         var message = document.createElement('div');
         var messages = document.getElementById('messages');
 
-        message.appendChild(text)
+        message.appendChild(text);
         messages.appendChild(message);
     }
   };
