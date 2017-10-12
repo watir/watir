ba5f9c4eb752ce68487a5edb238192f949ce3d5b
Justin Ko <jkotests@gmail.com>
2017-10-11 22:06:29 -0400

    Support multiple parameters in UserEditable#set!

--- lib/watir/user_editable.rb
+++ lib/watir/user_editable.rb
@@ -22,7 +22,9 @@ module Watir
     # @param [String] input_value
     #
 
-    def set!(input_value)
+    def set!(*args)
+      raise ArgumentError, "#set! does not support special keys, use #set instead" if args.any? { |v| v.kind_of?(::Symbol) }
+      input_value = args.join
       set input_value[0]
       element_call { execute_js(:setValue, @element, input_value[0..-2]) }
       append(input_value[-1])
