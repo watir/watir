d7e84f54582db26be8f306264085ccb908469b8e
Gijs Paulides <gijs.paulides@spritecloud.com>
2017-12-07 17:50:55 +0100

    Flash delay to 0.1 + remove white color at end of flash
    As discussed delay is now 0.1.
    Also I noted that at the end of a flash animation, it was changing the element background color to white. Very visible here:
    ```
    require 'watir'
    b = Watir::Browser.new
    b.goto 'http://training-page.testautomation.info/'
    b.a(:text => 'blog').flash
    ```

--- lib/watir/js_execution.rb
+++ lib/watir/js_execution.rb
@@ -43,11 +43,10 @@ module Watir
     # @return [Watir::Element]
     #
 
-    def flash(color: 'red', flashes: 5, delay: 0.01)
+    def flash(color: 'red', flashes: 5, delay: 0.1)
       background_color = style("backgroundColor")
       background_color = 'white' if background_color.empty?
       element_color = element_call { execute_js(:backgroundColor, @element) }.strip
-      element_color = 'white' if element_color.empty?
 
       (flashes * 2).times do |n|
         nextcolor = n.even? ? color : background_color
