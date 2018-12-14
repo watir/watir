53e25de5c5a50b18af70ee44edea790a77e643dc
Titus Fortner <titusfortner@gmail.com>
2018-12-14 14:56:45 -0500

    don't record coverage on Appveyor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,21 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'coveralls'
-require 'simplecov'
-require 'simplecov-console'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
-  Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::Console
-]
-SimpleCov.start do
-  add_filter %r{/spec/}
-  add_filter 'lib/watir/elements/html_elements.rb'
-  add_filter 'lib/watir/elements/svg_elements.rb'
-  add_filter 'lib/watir/legacy_wait.rb'
-  add_filter 'lib/watirspec'
-  refuse_coverage_drop
+if ENV['TRAVIS'] || ENV['COVERAGE']
+  require 'coveralls'
+  require 'simplecov'
+  require 'simplecov-console'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [Coveralls::SimpleCov::Formatter,
+                                                                  SimpleCov::Formatter::HTMLFormatter,
+                                                                  SimpleCov::Formatter::Console]
+  SimpleCov.start do
+    add_filter %r{/spec/}
+    add_filter 'lib/watir/elements/html_elements.rb'
+    add_filter 'lib/watir/elements/svg_elements.rb'
+    add_filter 'lib/watir/legacy_wait.rb'
+    add_filter 'lib/watirspec'
+    refuse_coverage_drop
+  end
 end
 
 require 'watir'
