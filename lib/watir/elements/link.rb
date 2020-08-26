b38307ceab2d8801dce8071c99d74dbf4aa1b51c
titusfortner <titusfortner@gmail.com>
2020-08-26 13:49:51 -0700

    html spec bug workarounds no longer needed

--- lib/watir/elements/link.rb
+++ lib/watir/elements/link.rb
@@ -3,13 +3,4 @@ module Watir
     alias link a
     alias links as
   end # Container
-
-  class Anchor < HTMLElement
-    #
-    # @todo temporarily add href attribute
-    #
-    # @see https://www.w3.org/Bugs/Public/show_bug.cgi?id=23192
-    #
-    attribute String, :href, :href
-  end # Anchor
 end # Watir
